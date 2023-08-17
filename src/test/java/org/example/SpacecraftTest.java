7e782e3dbb4d420362bc7c9a828b0c3123ab3d9e
Mohitur669 <mohitur669@gmail.com>
2023-08-17 14:41:15 +0530

    testing moveBackwar with testMoveBackward

--- src/test/java/org/example/SpacecraftTest.java
+++ src/test/java/org/example/SpacecraftTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SpacecraftTest {
@@ -35,6 +36,40 @@ public class SpacecraftTest {
         spacecraft = new Spacecraft(0, 0, 0, 'D');
         spacecraft.moveForward();
         assertEquals("(0, 0, -1)", spacecraft.getPosition());
+        assertEquals('D', spacecraft.getDirection());
+    }
+
+    // testing moving backward in all directions
+    @Test
+    public void testMoveBackward() {
+        Spacecraft spacecraft = new Spacecraft(0, 0, 0, 'N');
+        spacecraft.moveBackward();
+        assertEquals("(0, -1, 0)", spacecraft.getPosition());
+        assertEquals('N', spacecraft.getDirection());
+
+        spacecraft = new Spacecraft(0, 0, 0, 'S');
+        spacecraft.moveBackward();
+        assertEquals("(0, 1, 0)", spacecraft.getPosition());
+        assertEquals('S', spacecraft.getDirection());
+
+        spacecraft = new Spacecraft(0, 0, 0, 'E');
+        spacecraft.moveBackward();
+        assertEquals("(-1, 0, 0)", spacecraft.getPosition());
+        assertEquals('E', spacecraft.getDirection());
+
+        spacecraft = new Spacecraft(0, 0, 0, 'W');
+        spacecraft.moveBackward();
+        assertEquals("(1, 0, 0)", spacecraft.getPosition());
+        assertEquals('W', spacecraft.getDirection());
+
+        spacecraft = new Spacecraft(0, 0, 0, 'U');
+        spacecraft.moveBackward();
+        assertEquals("(0, 0, -1)", spacecraft.getPosition());
+        assertEquals('U', spacecraft.getDirection());
+
+        spacecraft = new Spacecraft(0, 0, 0, 'D');
+        spacecraft.moveBackward();
+        assertEquals("(0, 0, 1)", spacecraft.getPosition());
         assertEquals('D', spacecraft.getDirection());
     }
 }
