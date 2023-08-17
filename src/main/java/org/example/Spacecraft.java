8c7d1e530a56450cf55064bca74c59db71f0e402
Mohitur669 <mohitur669@gmail.com>
2023-08-17 14:32:22 +0530

    moveForward implementation

--- src/main/java/org/example/Spacecraft.java
+++ src/main/java/org/example/Spacecraft.java
@@ -4,10 +4,21 @@ public class Spacecraft {
     private int x, y, z;
     private char direction;
 
+    // Constructor
     public Spacecraft(int x, int y, int z, char direction) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.direction = direction;
+    }
+
+    // function to move the spacecraft in forward
+    public void moveForward() {
+        if (direction == 'N') y++;
+        else if (direction == 'S') y--;
+        else if (direction == 'E') x++;
+        else if (direction == 'W') x--;
+        else if (direction == 'U') z++;
+        else if (direction == 'D') z--;
     }
 }
