9f44d6dcb2b927b326315640c996f204751e6f80
Mohitur669 <mohitur669@gmail.com>
2023-08-17 14:29:42 +0530

    bulding Spacecraft Constructor

--- src/main/java/org/example/Spacecraft.java
+++ src/main/java/org/example/Spacecraft.java
@@ -1,5 +1,13 @@
 package org.example;
 
 public class Spacecraft {
+    private int x, y, z;
+    private char direction;
 
+    public Spacecraft(int x, int y, int z, char direction) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.direction = direction;
+    }
 }
